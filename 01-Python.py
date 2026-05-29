886807531aecbb2fe3e3074c19b01f77ee3ef081
Harsh <harshvankar631@gmail.com>
2026-05-29 21:22:59 +0530

    Add string and numeric operation examples
    Added examples demonstrating operations between strings and numeric values.

--- 01-Python.py
+++ 01-Python.py
@@ -11,3 +11,17 @@ a = None
 
 print(type(old))
 print(type(a))
+
+""""
+#String & Numeric values can operate together with* 
+A,B=2,3
+Txt="@"
+print(2*Txt*3)
+
+"""
+""""
+#String & String can operate with + 
+A,B = "2",3
+Txt="@"
+print((A+Txt)*3)
+"""
