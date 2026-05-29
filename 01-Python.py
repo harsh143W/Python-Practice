7461e86d776f40f335b4d1a24a6fd5e4087e6a24
Harsh <harshvankar631@gmail.com>
2026-05-29 20:43:38 +0530

    Add comment on data types and correct typo
    Added a comment about data types and fixed a typo.

---
+++ 01-Python.py
@@ -0,0 +1,13 @@
+name = "raj" # '=' is assignment operatoar
+age = 23
+price = 23.34
+#Date types 
+print(type(name))  
+print(type(age))
+print(type(price))
+
+old = False
+a = None
+
+print(type(old))
+print(type(a))
